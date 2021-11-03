5dec7489707f99e9ac5f17eb4bd380da7d42a35f
ramasubramanian99 <d.ramasubramanian.1997@gmail.com>
2021-11-02 20:20:49 -0400

    add remove blocked movies functionality

--- Capstone/extension/popup/popup.js
+++ Capstone/extension/popup/popup.js
@@ -1,27 +1,65 @@
 const enterMovie = document.getElementById('enterMovie')
 const movieInp = document.getElementById('movieInp')
-
+const blockedMovieList = document.getElementById('blockedMovieList')
 
 chrome.storage.local.get('blockedMovies', (result) => {
-    console.log('onload')
-    typeof(result) === 'object' ? chrome.storage.local.set({'blockedMovies': []}, ()=> {console.log('set variable')}) : null
+    if(typeof(result) === 'object') { 
+        if(result.blockedMovies === undefined) {
+            chrome.storage.local.set({'blockedMovies': []}, ()=> {
+                console.log('set variable')
+            });
+        }
+    }
+
+    result.blockedMovies.forEach(movie => {
+        addMovieNode(movie)
+    });
 });
 
 enterMovie.addEventListener('click', (e) => {
-    movie = movieInp.value
-    if(movieInp.value !== '') {
-        bm = [] 
-        console.log(bm)
+    let movie = movieInp.value
+    if(movieInp.value !== '' || movieInp.value !== ' ') {
+        let bm = [] 
         chrome.storage.local.get('blockedMovies', (res) => {
-            console.log('This is the storage '+res.blockedMovies)
             bm = res.blockedMovies
             bm.push(movie)
-            console.log(bm)
             chrome.storage.local.set({'blockedMovies': [...bm]}, () => {
                 console.log('Movie list updated')
-            })
-        })    
+            });
+
+            addMovieNode(movie)
+        });    
     }
      
     movieInp.value = ''
 });
+
+const removeSelectedMovie = (e, movieName) => {
+    chrome.storage.local.get('blockedMovies', (res) => {
+        let temp = res.blockedMovies;
+        let index = temp.indexOf(movieName)
+        if(index !== -1) {
+            temp.splice(index, 1)
+        }
+
+        chrome.storage.local.set({'blockedMovies': [...temp]}, () => {
+            blockedMovieList.removeChild(blockedMovieList.childNodes[index])
+        });
+    });
+}
+
+const addMovieNode = (movieName) => {
+    let closeTag = document.createElement('a')
+    let closeText = document.createTextNode('X')
+    closeTag.appendChild(closeText)
+    closeTag.setAttribute('class', 'removeMovie')
+    let temp = document.createElement('div')
+    let movieText = document.createTextNode(movieName)
+    let movieSpan =document.createElement('span')
+    movieSpan.appendChild(movieText)
+    movieSpan.setAttribute('name', movieName)
+    closeTag.addEventListener('click', (e) =>{removeSelectedMovie(e, movieName)})
+    temp.appendChild(movieSpan)
+    temp.appendChild(closeTag)
+    blockedMovieList.appendChild(temp)
+}
