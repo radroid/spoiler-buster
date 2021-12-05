6f1b94187c3057d414d14abc1ce79227f91bf572
ramasubramanian99 <d.ramasubramanian.1997@gmail.com>
2021-12-04 23:43:26 -0500

    add comments in popup.js file

--- Capstone/extension/popup/popup.js
+++ Capstone/extension/popup/popup.js
@@ -1,9 +1,16 @@
+// submit btn to add the movie to the blocked list
 const enterMovie = document.getElementById('enterMovie')
+// input to enter the movie
 const movieInp = document.getElementById('movieInp')
+// list of blocked movies 
 const blockedMovieList = document.getElementById('blockedMovieList')
+// extension on or off toggle
 const extnPower = document.getElementById('extnToggle')
 
+// initialize blockedmovie list everytime the pop is loaded
 chrome.storage.local.get('blockedMovies', (result) => {
+    // if the blockedmovie list from the local storage is still undefined ie its 
+    // the first time we are opening the extension define it as an empty list
     if(typeof(result) === 'object') { 
         if(result.blockedMovies === undefined) {
             chrome.storage.local.set({'blockedMovies': []}, ()=> {
@@ -12,23 +19,28 @@ chrome.storage.local.get('blockedMovies', (result) => {
         }
     }
 
+    // get the list of movies and display them in the popup
     result.blockedMovies.forEach(movie => {
         addMovieNode(movie)
     });
 });
 
+// initialize the extension power state everytime the pop opens up
 chrome.storage.local.get('isOn', (res) => {
+    // if the popup is opened for the first time the extension powerstate wont be defined so initialize it to off ie false
+    // else initialize whatever is the powerstate of the extension
     if(res.isOn === undefined) {
         chrome.storage.local.set({'isOn': false}, () => {
             console.log('extension power button initialized !!')
             setExtensionState(false)
         })
-    } else {
+    } else { 
         console.log('Intiialize'+ res.isOn)
         setExtensionState(res.isOn)
     }
 });
 
+// when a movie is entered check if the movie is empty append it to the blocked movie list and clear the input value
 enterMovie.addEventListener('click', (e) => {
     let movie = movieInp.value
     if(movie !== '' && movie !== ' ') {
@@ -49,6 +61,7 @@ enterMovie.addEventListener('click', (e) => {
     movieInp.value = ''
 });
 
+// function to remove the selected movie from the blocked movie list
 const removeSelectedMovie = (e, movieName) => {
     chrome.storage.local.get('blockedMovies', (res) => {
         let temp = res.blockedMovies;
@@ -63,6 +76,7 @@ const removeSelectedMovie = (e, movieName) => {
     });
 }
 
+// function to display the movie in the popup 
 const addMovieNode = (movieName) => {
     let closeTag = document.createElement('a')
     let closeText = document.createTextNode('X')
@@ -79,15 +93,16 @@ const addMovieNode = (movieName) => {
     blockedMovieList.appendChild(temp)
 }
 
+// function to toggle the extension on or off
 const setExtensionState = (state) => {
     if(state) {
         extnPower.setAttribute('checked', state)
     } else {
         extnPower.removeAttribute('checked')
-    }
-    
+    }  
 }
 
+// function to toggle the isOn state when the user toggles the state in the popup
 const toggleFilter = () => {
     chrome.storage.local.get('isOn', (res) => {
         console.log('here')
@@ -99,6 +114,7 @@ const toggleFilter = () => {
     });
 }
 
+// add an eventlistener to check for toggle of extension power
 extnPower.addEventListener('click', (e) => {
     toggleFilter()
 });
